3e9baa18eb472f635ddb0d3f4164338636d75659
Christopher Lis <christopher4lis@gmail.com>
2023-04-02 14:47:31 -0400

    feat: add normal attribute to shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,6 +1,7 @@
 precision highp float;
 varying vec2 vUv;
+varying vec3 vNormal;
 
 void main () {
-    gl_FragColor = vec4(vec3(vUv.x) ,1.0);
+    gl_FragColor = vec4(vNormal,1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,12 +1,15 @@
 attribute vec3 position;
 attribute vec2 uv;
+attribute vec3 normal;
 
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
 
 varying vec2 vUv;
+varying vec3 vNormal;
 
 void main () {
    vUv = uv;
+   vNormal = normal;
    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
