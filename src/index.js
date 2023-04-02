c321098123d9bb9fd4b388dd4b03c7babd4a26c6
Christopher Lis <christopher4lis@gmail.com>
2023-04-02 14:35:22 -0400

    refactor: clean up base three.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './css/style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-
 import vertex from './shaders/vertex.glsl'
 import fragment from './shaders/fragment.glsl'
 
-// Three Scene
-let scene, camera, renderer, animationId, controls
-let geometry, material, mesh
+let scene
+let camera
+let renderer
+let animationId
+let controls
+let cube
 
 function init() {
   scene = new THREE.Scene()
@@ -20,26 +22,14 @@ function init() {
   )
   camera.position.z = 1000
 
-  geometry = new THREE.BoxGeometry(200, 200, 200)
-  material = new THREE.RawShaderMaterial({
-    vertexShader: vertex,
-    fragmentShader: fragment,
-  })
-
-  mesh = new THREE.Mesh(geometry, material)
-  scene.add(mesh)
-
-  for (let i = -5; i <= 5; i++) {
-    const geometry = new THREE.BoxGeometry(200, 200, 200)
-    const material = new THREE.MeshBasicMaterial({
-      color: 0xffffff,
-      wireframe: true,
+  cube = new THREE.Mesh(
+    new THREE.BoxGeometry(200, 200, 200),
+    new THREE.RawShaderMaterial({
+      vertexShader: vertex,
+      fragmentShader: fragment,
     })
-
-    const mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
-    mesh.position.x = i * 400
-  }
+  )
+  scene.add(cube)
 
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(window.innerWidth, window.innerHeight)
@@ -50,11 +40,10 @@ function init() {
 
 function animate() {
   animationId = requestAnimationFrame(animate)
-
-  mesh.rotation.x += 0.04
-  mesh.rotation.y += 0.02
-
   renderer.render(scene, camera)
+
+  cube.rotation.x += 0.04
+  cube.rotation.y += 0.02
 }
 
 init()
