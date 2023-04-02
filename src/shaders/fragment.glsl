9cc18295b4c6172f2a90d191394199deba1381c6
Christopher Lis <christopher4lis@gmail.com>
2023-04-02 15:27:39 -0400

    feat: perlin noise on fragment

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,7 +1,11 @@
 precision highp float;
+#include utils/perlin;
+
+varying vec3 vPosition;
 varying vec2 vUv;
 varying vec3 vNormal;
 
 void main () {
-   gl_FragColor = vec4(vNormal,1.0);
+   float noise = perlinNoise(vec3(vPosition.z * 5.0));
+   gl_FragColor = vec4(vec3(noise) ,1.0);
 }
